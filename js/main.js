0cb212e91973c2ba8fc5a2e9df49383d2e6b299e
Karthik Dileep <karthikdileep003@gmail.com>
2022-07-31 15:13:41 +0530

    finished but don't know if the mails are coming or not, need to confirm that

--- js/main.js
+++ js/main.js
@@ -5,3 +5,76 @@ $(window).scroll(function () {
         $('.navbar').css('background', 'transparent');
     }
 })
+
+
+//carousel
+$(document).ready(function () {
+
+    $('.items').slick({
+        dots: true,
+        infinite: true,
+        speed: 800,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+
+        ]
+    });
+});
+
+
+const isValidEmail = (email) => {
+    const re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    return re.test(String(email).toLowerCase());
+};
+
+const isValidPhone = (phone) => {
+    const re = /^(?:(?:\+|0{0,2})91(\s*|[\-])?|[0]?)?([6789]\d{2}([ -]?)\d{3}([ -]?)\d{4})$/;
+    return re.test(String(phone).toLowerCase());
+};
+
+const form = document.querySelector("form");
+const firstName = document.getElementById("first-name");
+const lastName = document.getElementById("last-name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+
+form.addEventListener("submit", (e) => {
+    var array = [firstName, lastName, email, phone];
+
+    array.forEach(element => {
+        if (element === null || element === '') {
+            e.preventDefault();
+        }
+    });
+
+   
+});
+
+
+
